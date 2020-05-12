8e71cde4a6766634b65bce3bee07091c4209ea93
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-12 21:32:53 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -19,6 +19,7 @@ def speak_to_grandma(string)
     return "HUH?! SPEAK UP, SONNY!"
   elsif string == string.upcase
     return "HUH?! SPEAK UP, SONNY!"
-  elsif 
+  elsif string.upcase == string.upcase
+    return "NO, NOT SINCE 1938!"
   end
 end
