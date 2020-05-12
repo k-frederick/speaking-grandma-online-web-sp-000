2c64089f9489933494481cd468dafd9c0e0827fc
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-12 21:03:05 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,7 +13,7 @@
 # 'I LOVE YOU TOO PUMPKIN!'
 
 def speak_to_grandma(string)
-  if "I LOVE YOU GRANDMA!" == "I LOVE YOU GRANDMA!".upcase
+  if string == "I LOVE YOU GRANDMA!".upcase
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif "Hi Nana, how are you?" == "Hi Nana, how are you?".upcase
     puts "HUH?! SPEAK UP, SONNY!"
