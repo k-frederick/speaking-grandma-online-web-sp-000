7916002e7ab00aa0dc6b59b1d0bfcd72a49726d4
Kristina Frederick <kristinafrederick@icloud.com>
2020-05-12 21:00:16 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -17,7 +17,7 @@ def speak_to_grandma
     puts "I LOVE YOU TOO PUMPKIN!"
   elsif "Hi Nana, how are you?" == "Hi Nana, how are you?".upcase
     puts "HUH?! SPEAK UP, SONNY!"
-  elsif "Hi!".upcase
+  elsif "Hi!" == "Hi!".upcase 
     puts "HUH?! SPEAK UP, SONNY!"
   elsif
     puts "NO, NOT SINCE 1938!"
